a62703250874b2b5059b81af38b6d8beced34bf0
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-12-16 18:55:16 +0100

    Create Login component

---
+++ src/pages/Login.jsx
@@ -0,0 +1,18 @@
+const Login = () => {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Chat App</span>
+        <span className="title">Login</span>
+        <form>
+          <input required type="email" placeholder="email" />
+          <input required type="password" placeholder="password" />
+          <button>Sign in</button>
+        </form>
+        <p>You don't have an account? Register</p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
