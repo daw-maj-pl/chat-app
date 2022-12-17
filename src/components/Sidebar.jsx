9cec0c19f96fd35151c18d4bce4d9dec790e7620
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-12-17 10:49:02 +0100

    Create Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div>Navbar</div>
+      <div>Search</div>
+      <div>Chats</div>
+    </div>
+  );
+};
+
+export default Sidebar;
