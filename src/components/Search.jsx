919dafe74ccf229ea5fe1313a1e6abac60e5539f
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-12-18 16:15:55 +0100

    Create Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+const Search = () => {
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Find a user" />
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/14494902/pexels-photo-14494902.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
