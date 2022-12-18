3ec42d7c9c4d4c7c0b2ff318c01d6697194525e9
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-12-18 16:16:27 +0100

    Import and render Navbar and Search components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,11 @@
+import Navbar from './Navbar';
+import Search from './Search';
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div>Navbar</div>
-      <div>Search</div>
+      <Navbar />
+      <Search />
       <div>Chats</div>
     </div>
   );
