320788bfa9e579060c12cb41dea0e3cb26b5593b
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-12-21 21:31:48 +0100

    Import and render Input component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,7 @@ import Cam from '../img/cam.png';
 import Add from '../img/add.png';
 import More from '../img/more.png';
 import Messages from './Messages';
+import Input from './Input';
 
 const Chat = () => {
   return (
@@ -15,6 +16,7 @@ const Chat = () => {
         </div>
       </div>
       <Messages />
+      <Input />
     </div>
   );
 };
