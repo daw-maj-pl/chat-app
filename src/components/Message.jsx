75075204911f99b9e53796ca721912f181c010d0
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-12-22 08:45:11 +0100

    Implement Message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,22 @@
 const Message = () => {
-  return <div>Message</div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/14494902/pexels-photo-14494902.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img
+          src="https://images.pexels.com/photos/14494902/pexels-photo-14494902.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
