fafc6c123bc85216b3be9e643914b3ed3e2b8ec3
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-12-25 10:52:01 +0100

    Implement react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import './style.scss';
 
 function App() {
-  return <Register />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
